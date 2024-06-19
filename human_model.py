9ab210441cb7330c429a79143c45445616a08c17
lalalaoalla <avologina93@mail.ru>
2024-06-19 22:30:48 +0700

    Класс Human
    Я немного доделала класс Human, теперь они оба живут в одном доме
    
    ЧТО ЕЩЕ ДОБАВИТЬ: класс котов(ну да чтобы что-то добавить побаловаться а что), а также чтобы они между собой повзаимодействовали, как-то так, а я спать

--- human_model.py
+++ human_model.py
@@ -1,28 +1,26 @@
 #атрибуты человека - сытость, количество денег, количество еды, усталость
 #методы - играть, есть, работать, ходить в магазин
-#если сытость объекта < 0, то объект умирает
-#челоыеку надо прожить 365 дней
+# В общем - у них теперь общий бюджет и еда, поэтому из объектов класса убралось количество еды и денег
+# Пока что я еще не знаю можно ли сделать это как-то лучще..
 from termcolor import cprint
 
 class Human:
     
     def __init__(self, name):
-        self.money = 0#количество денег
-        self.count_food=10#количество еды, которую я могу поесть
         self.fullness=0# сытость
         self.name=name
         self.fatique=0#усталость
         self.house = None
     
     def __str__(self):
-        return f"Я {self.name}, у меня осталось еды - {self.count_food}, денег - {self.money} и я на столько сыт {self.fullness}, а устал {self.fatique}"
+        return f"Я {self.name}, я на столько сыт {self.fullness}, а устал {self.fatique}"
     
     def work(self):
-        if (self.money == 0):
-            self.money+=100
+        if (self.house.has_money == 0):
+            self.house.has_money+=100
             self.fatique+=10
             self.fullness-=20
-            cprint(f"Я - {self.name}, я заработал вот столько денег - {self.money}, а также устал - {self.fatique}, проголодался - {self.fullness}", color='yellow')
+            cprint(f"Я - {self.name}, я заработал вот столько денег - {self.house.has_money}, а также устал - {self.fatique}, проголодался - {self.fullness}", color='yellow')
     
     def play(self):
         if(self.fatique > 10):
@@ -30,22 +28,22 @@ class Human:
             cprint(f'Я - {self.name}, я отдохнул - {self.fatique}',color='cyan')    
 
     def shopping(self):
-        if(self.money>1):
-            while(self.money != 0):
-                self.count_food+=1
-                self.money-=10
-            cprint(f'Я {self.name}, я купил еду - {self.count_food}, у меня осталось столько денег - {self.money}', color='light_blue')    
+        if(self.house.has_money>1):
+            while(self.house.has_money != 0):
+                self.house.has_food+=1
+                self.house.has_money-=10
+            cprint(f'Я {self.name}, я купил еду - {self.house.has_food}, у меня осталось столько денег - {self.house.has_money}', color='light_blue')    
     
     def eat(self):
         if (self.fullness == 0):
-            if(self.count_food !=0):
-                self.count_food-=1
+            if(self.house.has_food !=0):
+                self.house.has_food-=1
                 self.fullness+=1
                 cprint(f"{self.name} поел",color='light_red')
     
     def go_into_the_house(self, house):
         self.house=house
-        cprint(f'Я заехал в этот дом!!!!!!! {self.house}', color='blue')
+        cprint(f'Я {self.name} заехал в этот дом!!!!!!! {self.house}', color='blue')
 
     def act(self):
         self.eat()
@@ -58,14 +56,19 @@ class House:
         self.has_food=10
         self.has_money=500
         self.addres=addres
+    def __str__(self):
+        return f'В доме осталось столько еды - {self.has_food}, столько денег - {self.has_money}, а вот мой адрес - {self.addres}'
 
 
 Andrey = Human('Andrey')
 Olga=Human('Olga')
 house=House('Советская 123')
-#for day in range (1,8):
- #   cprint(f"================day {day}==========================",color='red')
-  #  Andrey.act()
-   # Olga.act()
-    #print(Andrey,Olga)
-print(Andrey.go_into_the_house('Советская 123'))
+Andrey.go_into_the_house(house=house)
+Olga.go_into_the_house(house=house)
+for day in range (1,8):
+    cprint(f"================day {day}==========================",color='red')
+    Andrey.act()
+    Olga.act()
+    print(Andrey,Olga)
+    print(house)
+#print(Andrey.go_into_the_house('Советская 123'))
